1717f14eb9f29952be87dae7c0cf2614c34a3f2c
Amilcar Mula <amilcarmhula.am@gmail.com>
2024-12-17 11:16:46 +0200

    Actualizacao da componente equipe

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
+      
       <div>
         <Routes>
           <Route path="/" element={<HomePage/>}/>
--- src/components/equipe/Equipe.js
+++ src/components/equipe/Equipe.js
@@ -1,100 +1,94 @@
-import './equipeStyles.css'
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
+import './equipeStyles.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules'; // Import corrigido
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import me from '../../assets/1.jpg'
+import me from '../../assets/1.jpg';
 
 function EquipePage() {
-
-    const swiper = new Swiper('.swiper', {
-        modules: [Navigation, Pagination],
-        loop: true,
-        grabCursor: true,
-        spaceBetween: 30,
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-            clickable:true,
-            dynamicBullets:true,
-        },
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1
-            },
-            768: {
-                slidesPerView: 3
-            },
-            1024: {
-                slidesPerView: 4
-            }
-        }
-    });
-
-
     return (
         <>
-            <div className='container' onLoad={()=>swiper}>
-                <div className='swiper'>
-                        <div className="card-list swiper-wrapper">
+            <div className='container'>
+                <div className="card-list swiper-wrapper">
 
-                            <div className="card-item swiper-slide">
+                    <Swiper
+                        modules={[Navigation, Pagination]}
+                        loop={true}
+                        grabCursor={true}
+                        spaceBetween={30}
+                        pagination={{
+                            clickable: true,
+                            dynamicBullets: true,
+                        }}
+                        navigation={true}
+                        breakpoints={{
+                            0: { slidesPerView: 1 },
+                            768: { slidesPerView: 3 },
+                            1024: { slidesPerView: 4 },
+                        }}
+                    >
+
+
+                        <SwiperSlide>
+                            <div className="card-item">
                                 <img src={me} alt="User Image" className='user-image' />
                                 <h2 className='user-name'>Amilcar Mula</h2>
                                 <p className='user-profession'>Eng. Informático e de Telecomunicações</p>
                                 <button className='message-button'>Contacte-me</button>
                             </div>
+                        </SwiperSlide>
 
-                            <div className="card-item swiper-slide">
+                        <SwiperSlide>
+                            <div className="card-item">
                                 <img src={me} alt="User Image" className='user-image' />
                                 <h2 className='user-name'>Edmilson Mula</h2>
                                 <p className='user-profession'>Designer Gráfico e Marketing Digital</p>
                                 <button className='message-button'>Contacte-me</button>
                             </div>
+                        </SwiperSlide>
 
-                            <div className="card-item swiper-slide">
+                        <SwiperSlide>
+                            <div className="card-item">
                                 <img src={me} alt="User Image" className='user-image' />
                                 <h2 className='user-name'>Benildo Nhantumbo</h2>
                                 <p className='user-profession'>Gestor de Recursos Humanos</p>
                                 <button className='message-button'>Contacte-me</button>
                             </div>
+                        </SwiperSlide>
 
-                            <div className="card-item swiper-slide">
+                        <SwiperSlide>
+                            <div className="card-item">
                                 <img src={me} alt="User Image" className='user-image' />
                                 <h2 className='user-name'>Edson Nhantumbo</h2>
                                 <p className='user-profession'>Eng. Informático e de Telecomunicações</p>
                                 <button className='message-button'>Contacte-me</button>
                             </div>
+                        </SwiperSlide>
 
-                            <div className="card-item swiper-slide">
+                        <SwiperSlide>
+                            <div className="card-item">
                                 <img src={me} alt="User Image" className='user-image' />
                                 <h2 className='user-name'>Eurix Macamo</h2>
                                 <p className='user-profession'>Eng. de Tecnologias de Informação, Comunicação e Gestão</p>
                                 <button className='message-button'>Contacte-me</button>
                             </div>
+                        </SwiperSlide>
 
-                            <div className="card-item swiper-slide">
+                        <SwiperSlide>
+                            <div className="card-item">
                                 <img src={me} alt="User Image" className='user-image' />
                                 <h2 className='user-name'>Amilcar Mula, Jr.</h2>
                                 <p className='user-profession'>Eng. Informático e de Telecomunicações</p>
                                 <button className='message-button'>Contacte-me</button>
                             </div>
-                        </div>
-                    <div class="swiper-pagination"></div>
-                    <div class="swiper-button-prev"></div>
-                    <div class="swiper-button-next"></div>
+                        </SwiperSlide>
+
+                    </Swiper>
                 </div>
             </div>
         </>
-    )
-
+    );
 }
 
-export default EquipePage
+export default EquipePage;
