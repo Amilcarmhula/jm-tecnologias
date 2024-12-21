659518f13d1a8089328c0b6507189c0084bcf46d
Amilcar Mula <amilcarmhula.am@gmail.com>
2024-12-21 10:24:38 +0200

    Adicao da heroSection

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,6 +2,7 @@ import './homeStyles.css'
 import EquipePage from '../equipe/Equipe';
 import ContactoPage from '../contacto/Contacto';
 import ServicePage from '../service/Service';
+import logo from '../../assets/LOGOTIPO.png'
 
 function HomePage() {
     const estilo = {
@@ -11,9 +12,19 @@ function HomePage() {
 
     return (
         <>
-            <div style={estilo}>
+            {/* <div style={estilo}>
                 <h1> Soluções inteligentes para o sucesso de pequenas e médias empresas</h1>
                 <p>unimos tecnologia e inteligência humana para oferecer suporte excepcional e soluções personalizadas que impulsionam negócios com confiança e eficiência.</p>
+            </div> */}
+            <div className='hero-section'>
+                <div className='left-col'>
+                    <h1> Soluções inteligentes para o sucesso de pequenas e médias empresas</h1>
+                    <p>Unimos tecnologia e inteligência humana para oferecer suporte excepcional e soluções personalizadas que impulsionam negócios com confiança e eficiência.</p>
+                    <a href="#">Contacte-nos</a>
+                </div>
+                <div className='right-col'>
+                    <img src={logo} alt="Logotipo da empresa" />
+                </div>
             </div>
             <ServicePage />
             <EquipePage />
